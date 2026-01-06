2adf8bf4bef1b5f16dba40773ac647503d03c491
Zahraa Qawariq <zahradeeb98@gmail.com>
2026-01-06 12:36:32 +0200

    remove dublicte import

--- src/Components/index.js
+++ src/Components/index.js
@@ -6,5 +6,3 @@ export { default as Typed } from "./Typed";
 export { default as ProjectBox } from "./ProjectBox";
 export { default as PhotoAvatar } from "./PhotoAvatar";
 export { default as Loader } from "./Loader";
-export { default as Skills } from "./Skills";
-export { default as Typed } from "./Typed";
